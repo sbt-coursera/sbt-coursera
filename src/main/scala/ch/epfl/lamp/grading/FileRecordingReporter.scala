af50f71db80635a34117b3d9beeef47a4fbb99d5
Denys Shabalin <denys.shabalin@epfl.ch>
2015-04-13 15:32:57 +0200

    Add support for multiple test suites

--- src/main/scala/ch/epfl/lamp/grading/FileRecordingReporter.scala
+++ src/main/scala/ch/epfl/lamp/grading/FileRecordingReporter.scala
@@ -22,50 +22,28 @@ class FileRecordingReporter extends Reporter {
 
   private def record(entry: Entry) = writeOutfile(entry.toJson)
 
-  private def extractWeight(s: String): (String, Int) = {
-    val (num, rest) = s.span(c => c != '\n')
-    try {
-      (rest.drop(1), num.toInt)
-    } catch {
-      case e: NumberFormatException =>
-        sys.error("Could not extract weight from test name string\n" + s)
-    }
-  }
-
-  def apply(event: Event): Unit = {
-    event match {
-      /* We don't get a `TestStarting` for ignored tests, but we do get here
-       * one tests that use `pending`.
-       */
-      case e: SuiteStarting =>
-        val (_, weight) = extractWeight(e.suiteName)
-        record(Start(weight))
-      case e: SuiteCompleted =>
-        record(End())
-      case e: TestStarting =>
-        val (name, weight) = extractWeight(e.testName)
-        record(TestStart(name, weight))
-      case e: TestSucceeded =>
-        val (name, weight) = extractWeight(e.testName)
-        record(TestSuccess(name, weight))
-      case e: TestPending =>
-        val (name, weight) = extractWeight(e.testName)
-        record(TestSuccess(name, weight))
-      case e: TestFailed =>
-        val (name, weight) = extractWeight(e.testName)
-        record(TestFailure(name, weight, e.message + (e.throwable match {
-          case None => ""
-          case Some(testFailed: TestFailedException) => ""
-          case Some(thrown) =>
-            /* The standard output is captured by sbt and printed as
-             * `testing tool debug output` in the feedback.
-             */
-            println("[test failure log] test name: " + name)
-            println(exceptionString(thrown) + "\n\n")
-            "\n[exception was thrown] detailed error message in debug output section below"
-        })))
-      case _ => ()
-    }
+  def apply(event: Event): Unit = event match {
+    /* We don't get a `TestStarting` for ignored tests, but we do get here
+     * one tests that use `pending`.
+     */
+    case e: SuiteStarting => record(SuiteStart(e.suiteName))
+    case e: SuiteCompleted => record(SuiteEnd(e.suiteName))
+    case e: TestStarting => record(TestStart(e.testName))
+    case e: TestSucceeded => record(TestSuccess(e.testName))
+    case e: TestPending => record(TestSuccess(e.testName))
+    case e: TestFailed =>
+      record(TestFailure(e.testName, e.message + (e.throwable match {
+        case None => ""
+        case Some(testFailed: TestFailedException) => ""
+        case Some(thrown) =>
+          /* The standard output is captured by sbt and printed as
+           * `testing tool debug output` in the feedback.
+           */
+          println("[test failure log] test name: " + e.testName)
+          println(exceptionString(thrown) + "\n\n")
+          "\n[exception was thrown] detailed error message in debug output section below"
+      })))
+    case _ => ()
   }
 
   def exceptionString(e: Throwable): String =
--- src/main/scala/ch/epfl/lamp/grading/Report.scala
+++ src/main/scala/ch/epfl/lamp/grading/Report.scala
@@ -2,6 +2,7 @@ package ch.epfl.lamp.grading
 
 import java.io.{ File, PrintWriter }
 import scala.Predef.{ any2stringadd => _, _ }
+import scala.util.Try
 import scala.collection.mutable
 import scala.pickling.Defaults._
 import scala.pickling.json._
@@ -15,73 +16,83 @@ import scala.pickling.json._
  *  get a following report:
  *
  *    Seq(
- *      Start(3),
- *      TestStart("a", 1),
- *      TestSuccess("a", 1),
- *      TestStart("b", 2),
- *      TestFailure("b", 2, "FooBarException"),
- *      End()
+ *      SuiteStart("foo::3"),
+ *      TestStart("foo::a::1"),
+ *      TestSuccess("foo::a::1"),
+ *      TestStart("foo::a::2"),
+ *      TestFailure("foo::b::2", "FooBarException"),
+ *      SuiteEnd("foo::3")
  *    )
  *
- *  Due to matching start/end entries it's easy and report if test crashed midway.
+ *  Due to matching start/end records it's easy to tell if the thing crashed or not.
  */
 object Report {
-  final case class Start(totalWeight: Int) extends Entry
-  final case class End() extends Entry
-  final case class TestStart(testName: String, testWeight: Int) extends Entry
-  final case class TestSuccess(testName: String, testWeight: Int) extends Entry
-  final case class TestFailure(testName: String, testWeight: Int, msg: String) extends Entry
+  final case class SuiteStart(suiteId: SuiteId) extends Entry
+  final case class SuiteEnd(suiteId: SuiteId) extends Entry
+  final case class TestStart(testId: TestId) extends Entry
+  final case class TestSuccess(testId: TestId) extends Entry
+  final case class TestFailure(testId: TestId, msg: String) extends Entry
   // Entry has to be defined after subclasses so that
   // directSubclasses work out to generate pickler and unpickler
   sealed abstract class Entry { def toJson: String = this.pickle.value }
   object Entry { def fromJson(value: String) = value.unpickle[Entry] }
 
-  /** Replay the records from the report file and construct a summary. */
+  class StringSplitExtractor(splitter: String) {
+    def unapplySeq(str: String) = Array.unapplySeq(str.split(splitter))
+  }
+  object ToInt {
+    def unapply(s: String) = Try(s.toInt).toOption
+  }
+
+  /** suiteName::suiteWeight */
+  type SuiteId = String
+  object SuiteId extends StringSplitExtractor("::")
+
+  /** suiteName::testName::testWeight */
+  type TestId = String
+  object TestId extends StringSplitExtractor("::")
+
+  final case class Suite(val name: String, val weight: Int,
+    var complete: Boolean = false,
+    val tests: mutable.Map[String, Test] = mutable.Map.empty)
+  final case class Test(val name: String, val weight: Int,
+    var failure: Option[String])
+
+  /** Replay the event records from the file and reconstruct
+   *  a sequence of suites that they represent.
+   */
   def summarize(file: File) = {
     val lines = io.Source.fromFile(file).getLines
     val entries = lines.map(Entry.fromJson).toSeq
-
-    var complete = false
-    var totalWeight = 0
-    val started = mutable.Map.empty[String, TestStart]
-    var succeeded = List.empty[TestSuccess]
-    var failed = List.empty[TestFailure]
+    val suites = mutable.Map.empty[String, Suite]
     entries.foreach {
-      case Start(weight) =>
-        assert(totalWeight == 0)
-        totalWeight = weight
-      case End() =>
-        assert(!complete)
-        complete = true
-      case e @ TestStart(name, _) =>
-        started += name -> e
-      case e @ TestSuccess(name, _) =>
-        succeeded ::= e
-        started -= name
-      case e @ TestFailure(name, _, _) =>
-        failed ::= e
-        started -= name
+      case SuiteStart(SuiteId(name, ToInt(weight))) =>
+        suites += name -> Suite(name, weight)
+      case SuiteEnd(SuiteId(name, _)) =>
+        suites(name).complete = true
+      case TestStart(TestId(suiteName, name, ToInt(weight))) =>
+        suites(suiteName).tests +=
+          name -> Test(name, weight, failure = Some("test has been aborted"))
+      case TestSuccess(TestId(suiteName, name, _)) =>
+        suites(suiteName).tests(name).failure = None
+      case TestFailure(TestId(suiteName, name, _), msg: String) =>
+        suites(suiteName).tests(name).failure = Some(msg)
     }
-    assert(totalWeight > 0)
-
-    Summary(complete, totalWeight, started.values.toList, succeeded, failed)
+    Summary(suites.values.toList)
   }
 
   /**
    * Summary of the test report that contains test entries
    *  grouped by their status and computes feedback and score of the submission.
    */
-  final case class Summary(
-      complete: Boolean,
-      maxScore: Int,
-      unfinishedTests: List[TestStart],
-      succeededTests: List[TestSuccess],
-      failedTests: List[TestFailure]) {
-    def score: Int = succeededTests.map(_.testWeight).sum
+  final case class Summary(suites: List[Suite]) {
+    def score: Int = suites.map { _.tests.values.map { t => t.failure.fold(t.weight)(_ => 0) }.sum }.sum
+    def maxScore: Int = suites.map { _.weight }.sum
+
     def feedback: String = {
       val sb = new StringBuilder
       sb.append {
-        "Your solution achieved a testing score of %d out of %d.\n".format(score, maxScore)
+        "Your solution achieved a testing score of %d out of %d.\n\n".format(score, maxScore)
       }
       if (score == maxScore)
         sb.append("Great job!!!\n\n")
@@ -90,31 +101,21 @@ object Report {
                     |indicating the reason for the test failure and how many points
                     |you lost for each individual test.
                     |
-                    |""".stripMargin)
-        failedTests.foreach { e =>
-          sb.append(s"[Test Description] ${e.testName}")
-          sb.append(s"[Observed Error] ${e.msg}")
-          sb.append(s"[Lost Points] ${e.testWeight}")
-        }
-        sb.append("\n\n")
-      }
-      if (unfinishedTests.nonEmpty) {
-        sb.append("""Below are names of the tests that crashed the jvm or took too long
-                    |too complete. Such crashes can arise due to infinite non-terminitaing
+                    |Tests that were aborted took too long too complete or crashed the
+                    |JVM. Such crashes can arise due to infinite non-terminitaing
                     |loops or recursion (StackOverflowException) or excessive mamory
-                    |consumption (OutOfMemoryException). You can see more details in the
-                    |tool log.
+                    |consumption (OutOfMemoryException).
                     |
                     |""".stripMargin)
-        unfinishedTests.foreach { e =>
-          sb.append(s"[Test Description] ${e.testName}")
+        for {
+          s <- suites
+          t <- s.tests.values
+          msg <- t.failure
+        } {
+          sb.append(s"[Test Description] ${s.name}::${t.name}\n")
+          sb.append(s"[Observed Error] $msg\n")
+          sb.append(s"[Lost Points] ${t.weight}\n\n")
         }
-        sb.append("\n\n")
-      } else if (!complete) {
-        sb.append("""An internal error happened while testing your code. Please send your
-                   | entire feedback message to one of the teaching assistants.
-                   |
-                   |""".stripMargin)
       }
       sb.toString
     }
